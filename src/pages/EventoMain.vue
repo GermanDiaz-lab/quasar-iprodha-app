fd4f394f41ad94d81b1c4cdc68e378eb7407eaed
German <germandiaz09a2@gmail.cmo>
2023-06-25 20:49:25 -0300

    'evento'

---
+++ src/pages/EventoMain.vue
@@ -0,0 +1,133 @@
+<template>
+  <q-header reveal flat bordered class="bg-orange-8" height-hint="98">
+    <q-toolbar class="text-black glossy">
+      <q-btn flat round dense icon="menu" class="q-mr-sm" />
+      <q-separator dark vertical inset />
+      <q-btn stretch flat label="Link" />
+
+      <q-space />
+
+      <q-btn-dropdown stretch flat label="Dropdown">
+        <q-list>
+          <q-item-label header>Folders</q-item-label>
+          <q-item
+            v-for="n in 3"
+            :key="`x.${n}`"
+            clickable
+            v-close-popup
+            tabindex="0"
+          >
+            <q-item-section avatar>
+              <q-avatar icon="folder" color="secondary" text-color="white" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Photos</q-item-label>
+              <q-item-label caption>February 22, 2016</q-item-label>
+            </q-item-section>
+            <q-item-section side>
+              <q-icon name="info" />
+            </q-item-section>
+          </q-item>
+          <q-separator inset spaced />
+          <q-item-label header>Files</q-item-label>
+          <q-item
+            v-for="n in 3"
+            :key="`y.${n}`"
+            clickable
+            v-close-popup
+            tabindex="0"
+          >
+            <q-item-section avatar>
+              <q-avatar icon="assignment" color="primary" text-color="white" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Vacation</q-item-label>
+              <q-item-label caption>February 22, 2016</q-item-label>
+            </q-item-section>
+            <q-item-section side>
+              <q-icon name="info" />
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-btn-dropdown>
+      <q-separator dark vertical />
+      <q-btn stretch flat label="Link" />
+      <q-separator dark vertical />
+      <q-btn stretch flat label="Link" />
+    </q-toolbar>
+  </q-header>
+  <div class="q-pa-md">
+    <q-carousel
+      v-model="slide"
+      transition-prev="slide-right"
+      transition-next="slide-left"
+      navigation
+      animated
+      arrows
+      autoplay
+      infinite
+      control-color="primary"
+      class="rounded-borders text-black"
+    >
+      <template v-slot:navigation-icon="{ active, btnProps, onClick }">
+        <q-btn
+          v-if="active"
+          size="lg"
+          icon="home"
+          color="orange"
+          flat
+          round
+          dense
+          @click="onClick"
+        />
+        <q-btn
+          v-else
+          size="sm"
+          :icon="btnProps.icon"
+          color="black"
+          flat
+          round
+          dense
+          @click="onClick"
+        />
+      </template>
+      <q-carousel-slide name="style" class="column no-wrap flex-center">
+        <q-img src="..\assets\Fondo2.png" />
+      </q-carousel-slide>
+      <q-carousel-slide name="tv" class="column no-wrap flex-center">
+        <q-icon name="live_tv" color="primary" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="layers" class="column no-wrap flex-center">
+        <q-icon name="layers" color="primary" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="map" class="column no-wrap flex-center">
+        <q-img src="..\assets\logoevento.png" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+    </q-carousel>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    return {
+      slide: ref('style'),
+      lorem:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum, error dignissimos praesentium libero ab nemo.',
+    };
+  },
+};
+</script>
+
+<style></style>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,7 +2,7 @@ import { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
   {
-    path: '/',
+    path: '/main',
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
@@ -14,6 +14,11 @@ const routes: RouteRecordRaw[] = [
       { path: '/test', component: () => import('pages/GisTest.vue') },
       { path: '/test2', component: () => import('pages/GisTest2.vue') },
     ],
+  },
+  {
+    path: '/',
+    component: () => import('layouts/EventoLayout.vue'),
+    children: [{ path: '/', component: () => import('pages/EventoTest.vue') }],
   },
 
   // Always leave this as last one,
